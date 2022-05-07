981a3ca60df9ad0eb2a6c2b01a7667fad7cd92b3
cristalfield <subhajitbhattacharya1996@gmail.com>
2022-05-07 14:30:58 +0530

    First Commit

---
+++ src/main/java/com/automationlab/CodingAssignmentTestvagrant/CodingAssignmentTestvagrantApplication.java
@@ -0,0 +1,13 @@
+package com.automationlab.CodingAssignmentTestvagrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingAssignmentTestvagrantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingAssignmentTestvagrantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automationlab/CodingAssignmentTestvagrant/CodingAssignmentTestvagrantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automationlab.CodingAssignmentTestvagrant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingAssignmentTestvagrantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3cb4db6e6b5402d71eedf5ce8ce9c2f2c64370b4
cristalfield <subhajitbhattacharya1996@gmail.com>
2022-05-07 15:03:38 +0530

    Added dependency

--- src/test/java/com/automationlab/CodingAssignmentTestvagrant/TestClasses/CodingAssignmentTestvagrantApplicationTests.java
+++ src/test/java/com/automationlab/CodingAssignmentTestvagrant/TestClasses/CodingAssignmentTestvagrantApplicationTests.java
@@ -1,4 +1,4 @@
-package com.automationlab.CodingAssignmentTestvagrant;
+package com.automationlab.CodingAssignmentTestvagrant.TestClasses;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
